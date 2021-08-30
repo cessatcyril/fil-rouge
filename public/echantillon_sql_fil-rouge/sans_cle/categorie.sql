31cf99f9dc90b4cca3808665db273dbc0ed13f2f
cessatcyril <cessat.cyril@gmail.com>
2021-08-30 11:16:59 +0200

    deplacement fichier echantillon

--- public/echantillon_sql_fil-rouge/sans_cle/categorie.sql
+++
@@ -1,5 +0,0 @@
-insert into categorie (id, cat_nom) values (1, 'Guitare et basse');
-insert into categorie (id, cat_nom) values (2, 'Percussion et batterie');
-insert into categorie (id, cat_nom) values (3, 'Piano et clavier');
-insert into categorie (id, cat_nom) values (4, 'Instruments a vent');
-insert into categorie (id, cat_nom) values (5, 'Instruments classiques');
