1da2f27fae3a3b507767411ce0e2fc15dd9212b9
cessatcyril <cessat.cyril@gmail.com>
2021-08-30 10:01:49 +0200

    doc echantillon DB+merise

---
+++ public/echantillon_sql_fil-rouge/sans_cle/entreprise.sql
@@ -0,0 +1,10 @@
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (1, 'Bank of the Ozarks', 'Vietnam', 'Uyen Hung', '78199 Autumn Leaf Avenue', null, '270-47-5923');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (2, 'Williams-Sonoma, Inc.', 'Indonesia', 'Puncakmanis', '1120 Sloan Trail', null, '820-18-4616');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (3, 'Bank of N.T. Butterfield & Son Limited (The)', 'Brazil', 'Buíque', '24 Bay Plaza', '56520-000', '680-59-5771');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (4, 'PFSweb, Inc.', 'Russia', 'Biryulëvo', '5922 Weeping Birch Street', '142717', '646-74-2800');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (5, 'Guaranty Federal Bancshares, Inc.', 'Russia', 'Udachny', '199 Fulton Terrace', '678188', '382-52-0950');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (6, 'Clean Diesel Technologies, Inc.', 'Philippines', 'Kaligutan', '964 Service Street', '3320', '612-35-9177');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (7, 'Ramaco Resources, Inc.', 'China', 'Taocun', '788 Dovetail Place', null, '129-87-4291');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (8, 'Genworth Financial Inc', 'China', 'Haicheng', '80 Kim Alley', null, '490-70-5406');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (9, 'Thermo Fisher Scientific Inc', 'Mexico', 'La Loma', '138 Banding Hill', '92148', '574-03-3665');
+insert into entreprise (id, ent_nom, ent_pays, ent_ville, ent_adresse, ent_postal, ent_siret) values (10, 'Energy Focus, Inc.', 'Russia', 'Troitskoye', '1 Dunning Drive', '363704', '548-31-9624');
