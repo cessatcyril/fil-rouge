d015106f42543bd8b8a7987ffc644aee038bd588
cessatcyril <cessat.cyril@gmail.com>
2021-09-01 17:04:20 +0200

    panier 1.0

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,54 +2,135 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Produit;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PanierController extends AbstractController
 {
     /**
      * @Route("/panier/liste", name="panier_liste")
      */
-    public function panierListe(): Response
+    public function panierListe(Session $session): Response
     {
+        $panier = $session->get("panier");
+        // dd($panier);
+        if ($panier == null) $panier = [];
         return $this->render('panier/afficher.html.twig', [
             'controller_name' => 'PanierController',
+            "panier" => $panier
         ]);
     }
 
-    public function incrementer(): Response
+    /**
+     * @Route("/panier/raz", name="panier_raz")
+     */
+    public function panierRaz(SessionInterface $session): Response
     {
+        $session->invalidate();
+        $session->set("panier", []);
+
         return $this->render('panier/afficher.html.twig', [
             'controller_name' => 'PanierController',
+            "panier" => []
         ]);
     }
 
-    public function decrementer(): Response
+    /**
+     * @Route("/panier/ajouter/{id}", name="panier_ajouter")
+     */
+    public function ajouter(Produit $id, Session $session): Response
     {
-        return $this->render('panier/afficher.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
+
+        //$panierNon = $id->getId();
+        //$session->set($panierNon, $id);
+        $panier = $session->get("panier");
+        if ($panier == null) $panier = [];
+
+        $trouve = -1;
+        foreach ($panier as $i => $ligne) {
+            if ($ligne["id"] == $id->getId()) {
+                $trouve = $i;
+            }
+        }
+
+        if ($trouve == -1) {
+            $elt = [
+                "quantite" => 1,
+                "id" => $id->getId(),
+                "nom" => $id->getProProduit(),
+                "accroche" => $id->getProAccroche(),
+            ];
+            $panier[] = $elt;
+        } else {
+            $panier[$trouve]["quantite"]++;
+        }
+        $session->set("panier", $panier);
+
+        //dd($panier);
+        return $this->redirectToRoute("panier_liste");
+
+        // return $this->render('panier/afficher.html.twig', [
+        //     'controller_name' => 'PanierController',
+        //     "panier" => $panier
+        // ]);
     }
 
-    public function creer(): Response
+    /**
+     * @Route("/panier/supprimer/{produit}", name="panier_supprimer")
+     */
+    public function supprimer(Produit $produit, Session $session): Response
     {
-        return $this->render('panier/afficher.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
+        $panier = $session->get("panier");
+
+        $nouveau = [];
+        foreach ($panier as $ligne) {
+            if ($ligne["id"] != $produit->getId()) {
+                $nouveau[] = $ligne;
+            }
+        }
+        $session->set("panier", $nouveau);
+
+        return $this->redirectToRoute("panier_liste");
     }
 
-    public function supprimer(): Response
-    {
-        return $this->render('panier/afficher.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
-    }
-
+    /**
+     * @Route("/panier/modifier/{id}", name="panier_modifier")
+     */
     public function modifier(): Response
     {
         return $this->render('panier/afficher.html.twig', [
             'controller_name' => 'PanierController',
         ]);
+    }
+
+    /**
+     * @Route("/panier/reduire/{produit}", name="panier_reduire")
+     */
+    public function reduire(Produit $produit, Session $session): Response
+    {
+
+        //$panierNon = $id->getId();
+        //$session->set($panierNon, $id);
+        $panier = $session->get("panier");
+        if ($panier == null) $panier = [];
+
+        $trouve = -1;
+        foreach ($panier as $i => $ligne) {
+            if ($ligne["id"] == $produit->getId()) {
+                $trouve = $i;
+            }
+        }
+
+        if ($trouve != -1) {
+            $panier[$trouve]["quantite"]--;
+        }
+        $session->set("panier", $panier);
+
+        //dd($panier);
+        return $this->redirectToRoute("panier_liste");
     }
 }
